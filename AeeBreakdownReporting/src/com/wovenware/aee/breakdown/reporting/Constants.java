66c16ea1ca5bfef31088124d90bb96531acdb176
Nelson Perez <nperez@wovenware.com>
2013-06-06 10:14:32 -0400

    Collaboration Test

--- AeeBreakdownReporting/src/com/wovenware/aee/breakdown/reporting/Constants.java
+++ AeeBreakdownReporting/src/com/wovenware/aee/breakdown/reporting/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static class Bundles
 	{
-//		public static final String MESSAGES = "com.wovenware.aee.breakdown.reporting.MessageBundle";
+		public static final String MESSAGES = "com.wovenware.aee.breakdown.reporting.MessageBundle";
 //		public static final String ERRORS = "com.wovenware.aee.breakdown.reporting.ErrorBundle";
 		public static final String ENVIRONMENT = "com.wovenware.aee.breakdown.reporting.Environment";
 //		public static final String CONTENT = "com.wovenware.aee.breakdown.reporting.ContentBundle";
